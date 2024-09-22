c88ed9e5baad7105fc5eceeecacbedaf36671394
alsakib748 <alsakib748@gmail.com>
2024-09-22 16:26:49 +0600

    Customize Register and Login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,13 +30,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:100'],
+            'username' => ['required', 'string','lowercase','unique:users','min:3','max:50'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,7 @@
             <div class="container-fluid p-0">
                 <div class="card">
                     <div class="card-body">
-    
+
                         <div class="text-center mt-4">
                             <div class="mb-3">
                                 <a href="index.html" class="auth-logo">
@@ -35,45 +35,51 @@
                                 </a>
                             </div>
                         </div>
-    
+
                         <h4 class="text-muted text-center font-size-18"><b>Register</b></h4>
     
                         <div class="p-3">
                             <form class="form-horizontal mt-3" action="{{ route('register') }}" method="POST">
+                            @csrf    
     
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <input class="form-control" type="email" required="" placeholder="Email">
+                                        <input class="form-control" id="name" type="text" name="name" required="" placeholder="Name">
                                     </div>
                                 </div>
 
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <input class="form-control" type="email" required="" placeholder="Email">
+                                        <input class="form-control" id=username" type="text" name="username" required="" placeholder="UserName">
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id=email" type="email" name="email" required="" placeholder="Email">
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-3 row">
+                                    <div class="col-12">
+                                        <input class="form-control" id=password" type="password" name="password" required="" placeholder="Password">
                                     </div>
                                 </div>
     
                                 <div class="form-group mb-3 row">
                                     <div class="col-12">
-                                        <input class="form-control" type="text" required="" placeholder="Username">
+                                        <input class="form-control" id=password_confirmation" type="password" name="password_confirmation" required="" placeholder="Confirm Password">
                                     </div>
                                 </div>
     
-                                <div class="form-group mb-3 row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="password" required="" placeholder="Password">
-                                    </div>
-                                </div>
-    
-                                <div class="form-group mb-3 row">
+                                {{-- <div class="form-group mb-3 row">
                                     <div class="col-12">
                                         <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                            <label class="form-label ms-1 fw-normal" for="customCheck1">I accept <a href="#" class="text-muted">Terms and Conditions</a></label>
+                                            
                                         </div>
                                     </div>
-                                </div>
-    
+                                </div> --}}
+
                                 <div class="form-group text-center row mt-3 pt-1">
                                     <div class="col-12">
                                         <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Register</button>
@@ -82,7 +88,7 @@
     
                                 <div class="form-group mt-2 mb-0 row">
                                     <div class="col-12 mt-3 text-center">
-                                        <a href="pages-login.html" class="text-muted">Already have account?</a>
+                                        <a href="{{ route('login') }}" class="text-muted">Already have account?</a>
                                     </div>
                                 </div>
                             </form>
